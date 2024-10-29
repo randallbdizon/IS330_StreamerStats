4d136b9e966cff243f4b83702bb12c4969e8f5b7
Daruka Eon <darukaeon@gmail.com>
2024-10-29 11:20:13 -0700

    Added favicon.

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>List of Streamers</title>
+    <link rel="icon" href="Media/Images/CoughingCatIcon.png">
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
